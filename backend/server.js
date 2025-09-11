5e7b1408dd10fdb3132315e7696fb320e42cb81c
samiullahbahadur <bahadursamiullah@gmail.com>
2025-09-11 21:01:25 +0430

    added new api

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 app.get("/ping", (req, res) => res.json({ success: true, message: "pong" }));
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
