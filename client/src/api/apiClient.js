afa869f0057aa6825f6327b85835813c980d911f
samiullahbahadur <bahadursamiullah@gmail.com>
2025-09-11 13:08:55 +0430

    fix the frontend url

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: " https://bookstore-nodejs-74il.onrender.com/", // change to your backend API
-  withCredentials: true, // include cookies/sessions if needed
+  baseURL: "https://bookstore-nodejs-74il.onrender.com", // remove the space at start and optional trailing slash
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
