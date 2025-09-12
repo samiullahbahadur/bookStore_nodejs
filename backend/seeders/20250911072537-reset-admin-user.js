e1c73bdb7b618255fb03bc5ffa8f5fcf158ba8d1
samiullahbahadur <bahadursamiullah@gmail.com>
2025-09-12 14:27:37 +0430

    changes seeds

--- backend/seeders/20250911072537-reset-admin-user.js
+++ backend/seeders/20250911072537-reset-admin-user.js
@@ -1,33 +1,33 @@
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
 
-export default {
-  up: async (queryInterface, Sequelize) => {
-    // Delete ALL existing users
-    await queryInterface.bulkDelete("Users", null, {});
+// export default {
+//   up: async (queryInterface, Sequelize) => {
+//     // Delete ALL existing users
+//     await queryInterface.bulkDelete("Users", null, {});
 
-    // Hash password
-    const hashedPassword = await bcrypt.hash("admin@123", 10);
+//     // Hash password
+//     const hashedPassword = await bcrypt.hash("admin@123", 10);
 
-    // Insert a single clean admin
-    return queryInterface.bulkInsert("Users", [
-      {
-        name: "Super Admin",
-        username: "admin",
-        email: "admin@example.com",
-        password: hashedPassword,
-        isAdmin: true,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
-  },
+//     // Insert a single clean admin
+//     return queryInterface.bulkInsert("Users", [
+//       {
+//         name: "Super Admin",
+//         username: "admin",
+//         email: "admin@example.com",
+//         password: hashedPassword,
+//         isAdmin: true,
+//         createdAt: new Date(),
+//         updatedAt: new Date(),
+//       },
+//     ]);
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    // Rollback = remove this admin
-    return queryInterface.bulkDelete(
-      "Users",
-      { email: "admin@example.com" },
-      {}
-    );
-  },
-};
+//   down: async (queryInterface, Sequelize) => {
+//     // Rollback = remove this admin
+//     return queryInterface.bulkDelete(
+//       "Users",
+//       { email: "admin@example.com" },
+//       {}
+//     );
+//   },
+// };
